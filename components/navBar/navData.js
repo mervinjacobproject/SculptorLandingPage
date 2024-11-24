3fa5f2389e05fab8c1ea5c1bbe15606eb807b0b6
jackmervin <mervinofficial@gmail.com>
2024-11-24 13:56:28 +0530

    first Commit

---
+++ components/navBar/navData.js
@@ -0,0 +1,700 @@
+export const navData = [
+  // {
+  //   id: "au@2331",
+  //   itm: "Home1",
+  //   url: "#",
+  //   dropdown: true,
+  //   dropdown_itms: [
+  //     {
+  //       id: "du@011s",
+  //       dp_itm: "Home 1",
+  //       url: "/",
+  //     },
+  //     {
+  //       id: "du@012s",
+  //       dp_itm: "Home 2",
+  //       url: "/index-2",
+  //     },
+  //     {
+  //       id: "du@013s",
+  //       dp_itm: "Home 3",
+  //       url: "/index-3",
+  //     },
+  //     {
+  //       id: "du@014s",
+  //       dp_itm: "Home 4",
+  //       url: "/index-4",
+  //     },
+  //     {
+  //       id: "du@015s",
+  //       dp_itm: "Home 5",
+  //       url: "/index-5",
+  //     },
+  //     {
+  //       id: "du@016s",
+  //       dp_itm: "Home 6",
+  //       url: "/index-6",
+  //     },
+  //     {
+  //       id: "du@017s",
+  //       dp_itm: "Home 7",
+  //       url: "/index-7",
+  //     },
+  //     {
+  //       id: "du@018s",
+  //       dp_itm: "Home 8",
+  //       url: "/index-8",
+  //     },
+  //     {
+  //       id: "du@019s",
+  //       dp_itm: "Home 9",
+  //       url: "/index-9",
+  //     },
+  //     {
+  //       id: "du@020s",
+  //       dp_itm: "Home 10",
+  //       url: "/index-10",
+  //     },
+  //     {
+  //       id: "du@021s",
+  //       dp_itm: "Home 11",
+  //       url: "/index-11",
+  //     },
+  //   ],
+  // },
+  // {
+  //   id: "au@2380361",
+  //   itm: "Send",
+  //   url: "/index-4",
+  //   dropdown: false,
+  // },
+  // {
+  //   id: "au@2380362",
+  //   itm: "Receive",
+  //   url: "/index-5",
+  //   dropdown: false,
+  // },
+  // {
+  //   id: "au@23831",
+  //   itm: "Blog",
+  //   url: "#",
+  //   dropdown: true,
+  //   dropdown_itms: [
+  //     {
+  //       id: "du@0210s",
+  //       dp_itm: "Blog 1",
+  //       url: "/blog",
+  //     },
+  //     {
+  //       id: "du@022s",
+  //       dp_itm: "Blog 2",
+  //       url: "/blog-2",
+  //     },
+  //     {
+  //       id: "du@023s",
+  //       dp_itm: "Blog 3",
+  //       url: "/blog-3",
+  //     },
+  //     {
+  //       id: "du@024s",
+  //       dp_itm: "Blog 4",
+  //       url: "/blog-4",
+  //     },
+  //     {
+  //       id: "du@025s",
+  //       dp_itm: "Blog Single 1",
+  //       url: "/blog-single",
+  //     },
+  //     {
+  //       id: "du@026s",
+  //       dp_itm: "Blog Single 2",
+  //       url: "/blog-single-2",
+  //     },
+  //   ],
+  // },
+  // {
+  //   id: "au@238361",
+  //   itm: "Pages",
+  //   url: "#",
+  //   dropdown: true,
+  //   mega_menu: true,
+  //   dropdown_itms: [
+  //     {
+  //       id: "du@02001s",
+  //       dp_itm: "About Us",
+  //       url: "/about-us",
+  //     },
+  //     {
+  //       id: "du@02002s",
+  //       dp_itm: "Careers",
+  //       url: "/careers",
+  //     },
+  //     {
+  //       id: "du@02003s",
+  //       dp_itm: "Fees",
+  //       url: "/fees",
+  //     },
+  //     {
+  //       id: "du@02004s",
+  //       dp_itm: "Open Positions",
+  //       url: "/open-positions",
+  //     },
+  //     {
+  //       id: "du@02005s",
+  //       dp_itm: "Our Team",
+  //       url: "/our-team",
+  //     },
+  //     {
+  //       id: "du@02006s",
+  //       dp_itm: "Privacy Policy",
+  //       url: "/privacy-policy",
+  //     },
+  //     {
+  //       id: "du@02007s",
+  //       dp_itm: "Countries",
+  //       url: "/countries",
+  //     },
+  //     {
+  //       id: "du@02008s",
+  //       dp_itm: "Team Details",
+  //       url: "/team-details",
+  //     },
+  //     {
+  //       id: "du@02009s",
+  //       dp_itm: "Terms Conditions",
+  //       url: "/terms-conditions",
+  //     },
+  //     {
+  //       id: "du@020010s",
+  //       dp_itm: "Reviews",
+  //       url: "/reviews",
+  //     },
+  //     {
+  //       id: "du@020011s",
+  //       dp_itm: "How Works",
+  //       url: "/how-works",
+  //     },
+  //     {
+  //       id: "du@020012s",
+  //       dp_itm: "Faq",
+  //       url: "/faqs",
+  //     },
+  //     {
+  //       id: "du@020013s",
+  //       dp_itm: "Login 1",
+  //       url: "/login",
+  //     },
+  //     {
+  //       id: "du@020014s",
+  //       dp_itm: "Login 2",
+  //       url: "/login-2",
+  //     },
+  //     {
+  //       id: "du@020015s",
+  //       dp_itm: "Register 1",
+  //       url: "/register",
+  //     },
+  //     {
+  //       id: "du@020016s",
+  //       dp_itm: "Register 2",
+  //       url: "/register-2",
+  //     },
+  //     {
+  //       id: "du@020017s",
+  //       dp_itm: "Forgot Password 1",
+  //       url: "/forgot-password",
+  //     },
+  //     {
+  //       id: "du@020018s",
+  //       dp_itm: "Forgot Password 2",
+  //       url: "/forgot-password-2",
+  //     },
+  //     {
+  //       id: "du@020019s",
+  //       dp_itm: "Reset Password",
+  //       url: "/reset-password",
+  //     },
+  //     {
+  //       id: "du@020020s",
+  //       dp_itm: "Coming Soon",
+  //       url: "/coming-soon",
+  //     },
+  //     {
+  //       id: "du@020021s",
+  //       dp_itm: "404",
+  //       url: "/error",
+  //     },
+  //     {
+  //       id: "du@020022s",
+  //       dp_itm: "Contact",
+  //       url: "/contact",
+  //     },
+  //   ],
+  // },
+  // {
+  //   id: "au@238301",
+  //   itm: "Help",
+  //   url: "#",
+  //   dropdown: true,
+  //   dropdown_itms: [
+  //     {
+  //       id: "du@021Rs",
+  //       dp_itm: "Help Center 1",
+  //       url: "/help-center",
+  //     },
+  //     {
+  //       id: "du@022Rs",
+  //       dp_itm: "Help Center 2",
+  //       url: "/help-center-2",
+  //     },
+  //     {
+  //       id: "du@023Rs",
+  //       dp_itm: "Help Center Details",
+  //       url: "/help-center-details",
+  //     },
+  //   ],
+  // },
+  {
+    id: "au@2380361",
+    itm: "About Us",
+    url: "/index-4",
+    dropdown: false,
+  },
+  {
+    id: "au@238361",
+    itm: "Services",
+    url: "#",
+    dropdown: true,
+    mega_menu: true,
+    dropdown_itms: [
+      {
+        id: "du@02001s",
+        dp_itm: "PhD Topic Selection",
+        url: "/about-us",
+      },
+      {
+        id: "du@02002s",
+        dp_itm: "Problem Identification",
+        url: "/careers",
+      },
+      {
+        id: "du@02003s",
+        dp_itm: "Research Proposal",
+        url: "/fees",
+      },
+      {
+        id: "du@02004s",
+        dp_itm: "Pilot Study",
+        url: "/open-positions",
+      },
+      {
+        id: "du@02005s",
+        dp_itm: "PhD. Dissertation(Full)",
+        url: "/our-team",
+      },
+      {
+        id: "du@02006s",
+        dp_itm: "PhD. Dissertation(Part)",
+        url: "/privacy-policy",
+      },
+      {
+        id: "du@02007s",
+        dp_itm: "Phd Consultation",
+        url: "/countries",
+      },
+      {
+        id: "du@02008s",
+        dp_itm: " Viva Voce",
+        url: "/team-details",
+      },
+      {
+        id: "du@02009s",
+        dp_itm: "PhD Coursework Abstract Writing Help",
+        url: "/terms-conditions",
+      },
+      {
+        id: "du@020010s",
+        dp_itm: "Assignment",
+        url: "/reviews",
+      },
+      {
+        id: "du@020011s",
+        dp_itm: "Interim Report",
+        url: "/how-works",
+      },
+      {
+        id: "du@020012s",
+        dp_itm: "Exam Notes",
+        url: "/faqs",
+      },
+      {
+        id: "du@020013s",
+        dp_itm: "Synopsis Preparation",
+        url: "/login",
+      },
+      {
+        id: "du@020014s",
+        dp_itm: "Power Point",
+        url: "/login-2",
+      },
+      {
+        id: "du@020015s",
+        dp_itm: "Referances Collection",
+        url: "/register",
+      },
+      {
+        id: "du@020016s",
+        dp_itm: "Conceptual Framework",
+        url: "/register-2",
+      },
+      {
+        id: "du@020017s",
+        dp_itm: "Theoretical Framework",
+        url: "/forgot-password",
+      },
+      {
+        id: "du@020018s",
+        dp_itm: "Annotated Bibliography",
+        url: "/forgot-password-2",
+      },
+      {
+        id: "du@020019s",
+        dp_itm: "Theorem Development",
+        url: "/reset-password",
+      },
+      {
+        id: "du@020020s",
+        dp_itm: "Annotated Bibliography",
+        url: "/coming-soon",
+      },
+      {
+        id: "du@020021s",
+        dp_itm: "Theorem Development",
+        url: "/error",
+      },
+      {
+        id: "du@020022s",
+        dp_itm: "Gap Identification",
+        url: "/contact",
+      },
+      {
+        id: "du@02001s",
+        dp_itm: "Research Design",
+        url: "/about-us",
+      },
+      {
+        id: "du@02002s",
+        dp_itm: "Sampling",
+        url: "/careers",
+      },
+      {
+        id: "du@02003s",
+        dp_itm: "Sample Size",
+        url: "/fees",
+      },
+      {
+        id: "du@02004s",
+        dp_itm: "Power Calculation",
+        url: "/open-positions",
+      },
+      {
+        id: "du@02005s",
+        dp_itm: "Qualitative Methodology",
+        url: "/our-team",
+      },
+      {
+        id: "du@02006s",
+        dp_itm: "Quantitative Methodology",
+        url: "/privacy-policy",
+      },
+      {
+        id: "du@02007s",
+        dp_itm: "Primary Data Collection",
+        url: "/countries",
+      },
+      {
+        id: "du@02008s",
+        dp_itm: "Secondary Data Collection",
+        url: "/team-details",
+      },
+      {
+        id: "du@02009s",
+        dp_itm: "Quantitative Statisitics",
+        url: "/terms-conditions",
+      },
+      {
+        id: "du@020010s",
+        dp_itm: "Textual/Content Analysis",
+        url: "/reviews",
+      },
+      {
+        id: "du@020011s",
+        dp_itm: "Biostatistics",
+        url: "/how-works",
+      },
+      {
+        id: "du@020012s",
+        dp_itm: "Econometrics",
+        url: "/faqs",
+      },
+      {
+        id: "du@020013s",
+        dp_itm: "Big Data Analytics",
+        url: "/login",
+      },
+      {
+        id: "du@020014s",
+        dp_itm: "Software Programming",
+        url: "/login-2",
+      },
+      {
+        id: "du@020015s",
+        dp_itm: "Computer Programming",
+        url: "/register",
+      },
+      {
+        id: "du@020016s",
+        dp_itm: "Translation",
+        url: "/register-2",
+      },
+      {
+        id: "du@020017s",
+        dp_itm: "Transcription",
+        url: "/forgot-password",
+      },
+      {
+        id: "du@020018s",
+        dp_itm: "Plagiarism Correction",
+        url: "/forgot-password-2",
+      },
+      {
+        id: "du@020019s",
+        dp_itm: "Formatting & Referencing",
+        url: "/reset-password",
+      },
+      {
+        id: "du@020020s",
+        dp_itm: "Redrawing",
+        url: "/coming-soon",
+      },
+      {
+        id: "du@020021s",
+        dp_itm: "Manuscript Rewriting",
+        url: "/error",
+      },
+      {
+        id: "du@020022s",
+        dp_itm: "Manuscript Copyediting",
+        url: "/contact",
+      },
+      {
+        id: "du@02001s",
+        dp_itm: "Manuscript Peer Reviewing",
+        url: "/about-us",
+      },
+      {
+        id: "du@02002s",
+        dp_itm: "Manuscript Statistics",
+        url: "/careers",
+      },
+      {
+        id: "du@02003s",
+        dp_itm: "PhD Manuscript Formating Referencing",
+        url: "/fees",
+      },
+      {
+        id: "du@02004s",
+        dp_itm: "Manuscript Plagiarism Correction",
+        url: "/open-positions",
+      },
+      {
+        id: "du@02005s",
+        dp_itm: "Manuscript Editorial Comment Help",
+        url: "/our-team",
+      },
+      {
+        id: "du@02006s",
+        dp_itm: "Conference & Editorial Comment Help",
+        url: "/privacy-policy",
+      },
+      {
+        id: "du@02007s",
+        dp_itm: "Conference & Seminar Paper",
+        url: "/countries",
+      },
+      {
+        id: "du@02008s",
+        dp_itm: "Writing for a joumal",
+        url: "/team-details",
+      },
+      {
+        id: "du@02009s",
+        dp_itm: "Academic Statistic",
+        url: "/terms-conditions",
+      },
+      {
+        id: "du@020010s",
+        dp_itm: "Journal Manuscript Writing",
+        url: "/reviews",
+      },
+      {
+        id: "du@020011s",
+        dp_itm: "Research Methodology",
+        url: "/how-works",
+      },
+      {
+        id: "du@020012s",
+        dp_itm: "Animation",
+        url: "/faqs",
+      },
+      {
+        id: "du@020013s",
+        dp_itm: "PhD Animation Services",
+        url: "/login",
+      },
+    ],
+  },
+  {
+    id: "au@238361",
+    itm: "Industries",
+    url: "#",
+    dropdown: true,
+    mega_menu: true,
+    dropdown_itms: [
+      {
+        id: "du@02001s",
+        dp_itm: "About Us",
+        url: "/about-us",
+      },
+      {
+        id: "du@02002s",
+        dp_itm: "Careers",
+        url: "/careers",
+      },
+      {
+        id: "du@02003s",
+        dp_itm: "Fees",
+        url: "/fees",
+      },
+      {
+        id: "du@02004s",
+        dp_itm: "Open Positions",
+        url: "/open-positions",
+      },
+      {
+        id: "du@02005s",
+        dp_itm: "Our Team",
+        url: "/our-team",
+      },
+      {
+        id: "du@02006s",
+        dp_itm: "Privacy Policy",
+        url: "/privacy-policy",
+      },
+      {
+        id: "du@02007s",
+        dp_itm: "Countries",
+        url: "/countries",
+      },
+      {
+        id: "du@02008s",
+        dp_itm: "Team Details",
+        url: "/team-details",
+      },
+      {
+        id: "du@02009s",
+        dp_itm: "Terms Conditions",
+        url: "/terms-conditions",
+      },
+      {
+        id: "du@020010s",
+        dp_itm: "Reviews",
+        url: "/reviews",
+      },
+      {
+        id: "du@020011s",
+        dp_itm: "How Works",
+        url: "/how-works",
+      },
+      {
+        id: "du@020012s",
+        dp_itm: "Faq",
+        url: "/faqs",
+      },
+      {
+        id: "du@020013s",
+        dp_itm: "Login 1",
+        url: "/login",
+      },
+      {
+        id: "du@020014s",
+        dp_itm: "Login 2",
+        url: "/login-2",
+      },
+      {
+        id: "du@020015s",
+        dp_itm: "Register 1",
+        url: "/register",
+      },
+      {
+        id: "du@020016s",
+        dp_itm: "Register 2",
+        url: "/register-2",
+      },
+      {
+        id: "du@020017s",
+        dp_itm: "Forgot Password 1",
+        url: "/forgot-password",
+      },
+      {
+        id: "du@020018s",
+        dp_itm: "Forgot Password 2",
+        url: "/forgot-password-2",
+      },
+      {
+        id: "du@020019s",
+        dp_itm: "Reset Password",
+        url: "/reset-password",
+      },
+      {
+        id: "du@020020s",
+        dp_itm: "Coming Soon",
+        url: "/coming-soon",
+      },
+      {
+        id: "du@020021s",
+        dp_itm: "404",
+        url: "/error",
+      },
+      {
+        id: "du@020022s",
+        dp_itm: "Contact",
+        url: "/contact",
+      },
+    ],
+  },
+  {
+    id: "au@2380361",
+    itm: "Pricing",
+    url: "/index-4",
+    dropdown: false,
+  },
+  {
+    id: "au@2380361",
+    itm: "Help Guide",
+    url: "/index-4",
+    dropdown: false,
+  },
+  {
+    id: "au@2380361",
+    itm: "Contact Us",
+    url: "/index-4",
+    dropdown: false,
+  },
+  {
+    id: "au@2380361",
+    itm: "Hire a research Assistant",
+    url: "/index-4",
+    dropdown: false,
+  },
+];
